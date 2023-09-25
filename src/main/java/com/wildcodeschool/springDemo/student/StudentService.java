8767f3e3c563e8281e3550691452452a1cdb8144
luthfi.pratomo@telekom.de <luthfi.pratomo@telekom.de>
2023-09-25 14:29:36 +0200

    post mapping

--- src/main/java/com/wildcodeschool/springDemo/student/StudentService.java
+++ src/main/java/com/wildcodeschool/springDemo/student/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -30,4 +31,11 @@ public class StudentService {
 //        );
     }
 
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepo.findStudentByEmail(student.getEmail());
+        if (studentByEmail.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepo.save(student);
+    }
 }
